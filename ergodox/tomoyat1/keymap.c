4245d3cba6c6fddfa64375cc1fa03fcf42e9bf01
Tomoya Tabuchi <t@tomoyat1.com>
2017-12-31 11:49:20 +0900

    Move LALT in gaming layout

--- ergodox/tomoyat1/keymap.c
+++ ergodox/tomoyat1/keymap.c
@@ -42,22 +42,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = KEYMAP(
                 //left hand
                 KC_EQUAL,       KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_ESC,
-                KC_GRV,         KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_LBRC,
+                KC_GRV,         KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       LGUI(LSFT(KC_ENT)),
                 KC_LCTL,        KC_A,       KC_S,       KC_D,       KC_F,       KC_G,
-                KC_LSFT,        KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_LSFT,
+                KC_LSFT,        KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       MEH(KC_SPC),
                 MO(2),          KC_LGUI,    KC_LALT,    TG(3),      TG(4),
                                 KC_DEL,     KC_INS,
                                            KC_HYPR,
-                       KC_SPC,  KC_BSPC,    KC_LSFT,
+                       KC_SPC, KC_BSPC,MEH(KC_SPC),
                 //right hand
                 KC_EQUAL,   KC_6,       KC_7,       KC_8,       KC_9,       KC_0,           KC_MINUS,
-                KC_RBRC,    KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,           KC_BSLS,
+                LGUI(KC_ENT),KC_Y,      KC_U,       KC_I,       KC_O,       KC_P,           KC_BSLS,
                             KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,        KC_QUOT,
-                KC_RSFT,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,        KC_LSFT,
+                KC_TAB,     KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,        KC_LSFT,
                                         KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,         MO(1),
-                KC_RALT,    CTL_T(KC_ESCAPE),
+                KC_RALT,    CTL_T(KC_ESC),
                 KC_MEH,
-                KC_RSFT,GUI_T(KC_TAB),     SFT_T(KC_ENTER)),
+                KC_TAB, GUI_T(KC_ENT), KC_RSFT),
 
   // Layer 1:  Symbols
   [1] = KEYMAP(
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_TRNS,        KC_TRNS,    KC_LALT,    TO(0),      KC_B,
                                 KC_M,       KC_TRNS,
                                             KC_TRNS,
-                      KC_SPC,    KC_TRNS,   KC_TRNS,
+                      KC_SPC,    KC_ENTER,  KC_LALT,
                 //right hand
                 KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS,
                 KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS,
