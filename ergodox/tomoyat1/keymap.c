c3ef496b7d96284b69f4869631189a7bef775d21
Tomoya Tabuchi <t@tomoyat1.com>
2018-01-02 23:01:20 +0900

    Add question mark to symbols layer

--- ergodox/tomoyat1/keymap.c
+++ ergodox/tomoyat1/keymap.c
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_TRNS,    KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,         KC_F11,
                 KC_TRNS,    KC_BSLS,    KC_7,       KC_8,       KC_9,       KC_ASTR,        KC_F12,
                             KC_DOWN,    KC_4,       KC_5,       KC_6,       KC_PLUS,        KC_TRNS,
-                KC_TRNS,    KC_AMPR,    KC_1,       KC_2,       KC_3,       KC_BSLS,        KC_TRNS,
+                KC_TRNS,    KC_AMPR,    KC_1,       KC_2,       KC_3,       KC_BSLS,        KC_QUES,
                                         KC_TRNS,    KC_0,       KC_DOT,     KC_EQUAL,       KC_TRNS,
                 KC_TRNS,    KC_TRNS,
                 KC_TRNS,
