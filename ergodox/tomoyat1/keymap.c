e88e27d44fa47d27810acebc9dd37e3dba3a6d00
Tomoya Tabuchi <t@tomoyat1.com>
2018-01-05 20:17:58 +0900

    Add layer 1 toggle

--- ergodox/tomoyat1/keymap.c
+++ ergodox/tomoyat1/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                            KC_HYPR,
                        KC_SPC, KC_BSPC,MEH(KC_SPC),
                 //right hand
-                KC_EQUAL,   KC_6,       KC_7,       KC_8,       KC_9,       KC_0,           KC_MINUS,
+                TG(1),       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,           KC_MINUS,
                 LGUI(KC_ENT),KC_Y,      KC_U,       KC_I,       KC_O,       KC_P,           KC_BSLS,
                             KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,        KC_QUOT,
                 KC_TAB,     KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,        KC_LSFT,
