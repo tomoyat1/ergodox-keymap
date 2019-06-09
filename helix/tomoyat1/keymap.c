c761a7a781989e6d0bbbbc7189de99e951615e11
Tomoya Tabuchi <t@tomoyat1.com>
2019-06-09 18:08:36 +0900

    Add layer with numbers on row 2

--- helix/tomoyat1/keymap.c
+++ helix/tomoyat1/keymap.c
@@ -187,11 +187,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       TO(0),   KC_AMPR, KC_ASTR, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL,  KC_TRNS, KC_TRNS, KC_TRNS, KC_0,    KC_DOT,  KC_EQUAL,KC_TRNS \
       ),
 
-  /* Mouse keys
+  /* Numbers and Function
    * ,-----------------------------------------.             ,-----------------------------------------.
-   * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  | Bksp |
+   * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |             |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Tab  |   '  |   ,  | MS_UP|   P  |   Y  |             |   F  |   G  | WH_UP|   R  |   L  | Del  |
+   * | Tab  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   7  |   8  |   9  |   0  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Ctrl |   A  | MS_L | MS_DN| MS_R |   I  |             |   D  | WH_L | WH_DN| WH_R |   S  |  /   |
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
@@ -201,8 +201,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_MOUSE] = LAYOUT( \
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_MPRV, KC_MPLY, KC_MSTP, KC_MNXT, RESET, \
-      KC_TRNS, KC_TRNS, KC_COMM, KC_MS_U, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_WH_U, KC_R,    KC_L,    KC_TRNS, \
+      KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
+      KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12, \
       KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,                   KC_TRNS, KC_WH_L, KC_WH_D, KC_WH_R, KC_S,    KC_TRNS, \
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_BTN3, KC_TRNS, KC_M,    KC_W,    KC_V,    KC_Z,    KC_TRNS, \
       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC,  KC_BTN2, KC_BTN1, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS \
