13cfc9e921901db5f9c461943c55580fc0728308
Tomoya Tabuchi <t@tomoyat1.com>
2022-11-04 17:35:58 +0900

    Bump to 0.18.16
    * Fix ergodox_ez build

--- ergodox/tomoyat1/keymap.c
+++ ergodox/tomoyat1/keymap.c
@@ -219,19 +219,6 @@ const uint16_t PROGMEM fn_actions[] = {
   [1] = ACTION_LAYER_TAP_TOGGLE(1)
 };
 
-// leaving this in place for compatibilty with old keymaps cloned and re-compiled.
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-      switch(id) {
-        case 0:
-        if (record->event.pressed) {
-          SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-        }
-        break;
-      }
-    return MACRO_NONE;
-};
-
 void matrix_init_user(void) {
 #ifdef RGBLIGHT_COLOR_LAYER_0
   rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
@@ -522,7 +509,7 @@ void matrix_scan_user(void) {
   }
 }
 
-uint32_t layer_state_set_user(uint32_t state) {
+layer_state_t layer_state_set_user(layer_state_t state) {
 
     uint8_t layer = biton32(state);
 
